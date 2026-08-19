faf2d4740ed5c7fd4f0ff1d819306122d62aae9b
Rafael-Yuji <rafaelyuji2011@gmail.com>
2026-08-19 14:48:07 -0300

    introdução ao JS

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Portal de notícias carregado com sucesso!");
+
+let nomeVisitante = prompt("Qual é o seu nome?");
+alert("Olá, " + nomeVisitante + "! Bem vindo ao Portal ETEC News.");
+console.log("Visitante ativo: " + nomeVisitante);
+
+let desejaNoticias = confirm("Deseja receber notificações sobre novas matérias?");
+console.log("Aceitou receber notificações: " + desejaNoticias);
